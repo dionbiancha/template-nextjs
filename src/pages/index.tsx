5fb4804557b2d1e452b917fce7665e4ee444bf54
Dionei Bianchati <dion.biancha@gmail.com>
2021-04-05 18:40:21 -0300

    NextJS and TypeScript

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+export const Home = (): JSX.Element => {  
+  return (  
+    <>  
+      <h1>Blog</h1>  
+      <p>Iniciando o blog com Next e Typescript</p>  
+    </>  
+  );  
+};  
+  
+export default Home;  
